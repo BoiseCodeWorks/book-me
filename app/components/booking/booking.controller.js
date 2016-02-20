5895eebb39e751218a79178b3b7b337ec4c50079
Overall, Matthew <overallm@medamerica.com>
2016-02-20 12:31:39 -0700

    init commit

---
+++ app/components/booking/booking.controller.js
@@ -0,0 +1,11 @@
+(function() {
+'use strict';
+
+	angular
+		.module('app')
+		.controller('BookingController', BookingController);
+
+	function BookingController($scope, DataService) {
+		$scope.name = "Booking Controller..."
+	}
+})();
